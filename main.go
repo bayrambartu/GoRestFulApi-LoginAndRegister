d7e3e34acbdd531d4bb3ba830d32306ad571b475
bayram bartu <bayram213062@ogr.duzce.edu.tr>
2024-11-18 03:21:04 +0300

    login
    done!!!

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
+	"module/helpers"
 	"net/http"
-    "github.com/bayrambartu/GoRestFulApi-LoginAndRegister/helpers"
 )
 
 func main() {
 
-	uName,email,pwd,pwdConfirm := "","","","",
+	uName, email, pwd, pwdConfirm := "", "", "", ""
 
 	mux := http.NewServeMux()
 	// SignUp
@@ -18,40 +18,57 @@ func main() {
 
 		uName = r.FormValue("username")
 		email = r.FormValue("email")
-		pwd = r.FormValue("pwd")
-		pwdconfirm = r.FormValue("confirm")	
+		pwd = r.FormValue("password")
+		pwdConfirm = r.FormValue("confirm")
 
-		uNameCheck := helper.IsEmpty(uName)
-		emailCheck := helper.IsEmpty(email)
-		pwdCheck := helper.IsEmpty(pwd)
-		pwdConfirmCheck := helper.IsEmpty(pwdConfirm)
+		uNameCheck := helpers.IsEmpty(uName)
+		emailCheck := helpers.IsEmpty(email)
+		pwdCheck := helpers.IsEmpty(pwd)
+		pwdConfirmCheck := helpers.IsEmpty(pwdConfirm)
 
-		if uNameCheck 	|| emailCheck || pwdCheck ||pwdConfirmCheck{
-			fmt.Fprint(w,"ErrorCode is -10:There is empty data. ")
+		if uNameCheck || emailCheck || pwdCheck || pwdConfirmCheck {
+			fmt.Fprintf(w, "ErrorCode is -10:There is empty data. ")
 			return
 		}
-		if pwd == pwdConfirm{
-			// --> veritabanına kullanıcıyı kaydet.. 
-			fmt.Fprintf(w,"Registration succesful!")
-		}else{
-			fmt.Fprintln(w,"password information must be the same")
+		if pwd == pwdConfirm {
+			// --> veritabanına kullanıcıyı kaydet..
+			fmt.Fprintf(w, "Registration succesful!")
+		} else {
+			fmt.Fprintln(w, "password information must be the same")
 		}
-
-		
-
-
-		
+		/**/
 	})
 
 	// Login
 
 	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 
+		r.ParseForm()
+
+		email = r.FormValue("email")
+		pwd = r.FormValue("password")
+
+		emailCheck := helpers.IsEmpty(email)
+		pwdCheck := helpers.IsEmpty(pwd)
+
+		if emailCheck || pwdCheck {
+			fmt.Fprintf(w, "There is empty data.")
+			return
+		}
+		dbPwd := "12345!*."
+		dbEmail := "bb@gmail.com"
+
+		if email == dbEmail && pwd == dbPwd {
+			fmt.Fprintln(w, "Login succesful.")
+		} else {
+			fmt.Fprintln(w, "Login failed")
+		}
 	})
-	http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println("Sunucu başlatılamadı:", err)
+	}
 
-}
-
-// for key, value := range r.Form {
-		// 	fmt.Printf("%s = %s \n", key, value)
-		// }
+} // for key, value := range r.Form {
+// 	fmt.Printf("%s = %s \n", key, value)
+// }
